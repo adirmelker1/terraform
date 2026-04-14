ed24c6bf8f6b257446c0adce0d23b9947c8eda38
adir395 <adirmelker1@gmail.com>
2026-04-14 22:59:04 +0300

    aa

--- main.tf
+++ main.tf
@@ -1,13 +1,131 @@
-provider "aws" {
-  region     = "eu-north-1"
+provider "aws" {}
+
+variable vpc_cidr {
+}
+
+variable subnet_cidr {
+}
+
+variable env {
+}
+
+variable availability_zone {
+
+}
+
+variable env_prefix {
+
+}
+
+variable instance_type {
+
+}
+
+variable my_ip {
+
+}
+
+variable public_key_location {
+
 }
 
 resource "aws_vpc" "development_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
 }
 
 resource "aws_subnet" "development_subnet" {
   vpc_id            = aws_vpc.development_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-north-1a"
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.availability_zone
+  tags = {
+    Name = "${var.env_prefix}-subnet"
+  }
+}
+
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.development_vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.development_vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_default_security_group" "default_sg" {
+  vpc_id = aws_vpc.development_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+}
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+  egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      prefix_list_ids = []
+  }
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "myapp-server-kp"
+  public_key = file(var.public_key_location)
+
+}
+
+resource "aws_instance" "myapp-server" {
+  ami           = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.development_subnet.id
+  security_groups = [aws_default_security_group.default_sg.id]
+  availability_zone = var.availability_zone
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+
 }
